8ec7225b3ea7e4d2444358ddfd58e8db1e826210
Chris Eun <eunchristopher@gmail.com>
2022-11-18 19:41:17 -0800

    game practice using javascript, call back example

--- index.js
+++ index.js
@@ -2,8 +2,80 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
+character.style.zIndex = '1'
+let direction = null;
+let x = 100;
+let y = 250;
 
+function moveCharacter(){
+if (direction === 'west'){
+  x = x-1
+}
+if (direction === 'east'){
+  x = x+1
+}
+if (direction === 'south'){
+  y = y-1
+}
+if (direction === 'north'){
+  y = y+1
+}
+character.style.left = x + 'px'
+character.style.bottom = y + 'px'
+}
+
+setInterval(moveCharacter, 1)
+
+document.addEventListener('keydown', function(e){
+  if(e.repeat) return;
+
+  if(e.key === 'ArrowLeft'){
+    direction = 'west'
+    character.src = 'assets/green-character/west.gif'
+  }
+    if(e.key === 'ArrowRight'){
+    direction = 'east'
+    character.src = 'assets/green-character/east.gif'
+
+  }
+    if(e.key === 'ArrowDown'){
+    direction = 'south'
+    character.src = 'assets/green-character/south.gif'
+  }
+    if(e.key === 'ArrowUp'){
+    direction = 'north'
+    character.src = 'assets/green-character/north.gif'
+  }
+
+})
+
+document.addEventListener('keyup', function(e){
+  direction = null
+  character.src = 'assets/green-character/static.gif'
+})
+
+
+
+// const character = newImage('assets/green-character/static.gif')
+// if (direction === null)
+//   character.src = 'assets/green-character/static.gif'
+// }
+// if (direction === 'west'){
+//   character.src = 'assets/green-character/west.gif'
+// }
+// if (direction === 'east'){
+//  character.src = 'assets/green-character/east.gif'
+// }
+// if (direction === 'south'){
+//   character.src = 'assets/green-character/south.gif'
+// }
+// if (direction === 'north'){
+//  character.src = 'assets/green-character/north.gif'
+// }
+
+
+
+move(character).withArrowKeys(100, 250)
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
